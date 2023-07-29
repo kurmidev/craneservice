e7359a65c688d9e4da0b3c3562d2202a50c0972c
chandrapliquiloans <112621585+chandrapliquiloans@users.noreply.github.com>
2023-07-29 18:33:56 +0530

    Payment changes

--- views/client/print-invoice.php
+++ views/client/print-invoice.php
@@ -5,7 +5,7 @@ use yii\bootstrap5\Html;
 
 $i = 1;
 $total = $base_amount = $tax = 0;
-$f = new NumberFormatter("en", NumberFormatter::SPELLOUT);
+$f = new NumberFormatter("in", NumberFormatter::SPELLOUT);
 $bank = !empty($model->client->company->banks[1]) ? $model->client->company->banks[1] : (!empty($model->client->company->banks[0]) ? $model->client->company->banks[0] : []);
 
 ?>
